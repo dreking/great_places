56201f0d743478deee3488889bdf9125ca04a2f9
Leandre <leajapo@gmail.com>
2019-11-19 11:41:20 +0200

    feat:can add location and convert it into address, then fetch it

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/place.dart';
 import '../providers/great_places.dart';
 import '../widgets/image_input.dart';
 import '../widgets/location_input.dart';
@@ -16,13 +17,20 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File _pickedImage;
+  PlaceLocation _pickedLocation;
 
   void _selectImage(File pickedImage) {
     _pickedImage = pickedImage;
   }
 
+  void _selectPlace(double lat, double lng) {
+    _pickedLocation = PlaceLocation(latitude: lat, longitude: lng);
+  }
+
   void _savePlace(BuildContext context) {
-    if (_titleController.text.isEmpty || _pickedImage == null) {
+    if (_titleController.text.isEmpty ||
+        _pickedImage == null ||
+        _pickedLocation == null) {
       showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
@@ -40,7 +48,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
       );
     } else {
       Provider.of<GreatPlaces>(context, listen: false)
-          .addPlace(_titleController.text, _pickedImage);
+          .addPlace(_titleController.text, _pickedImage, _pickedLocation);
       Navigator.of(context).pop();
     }
   }
@@ -66,7 +74,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                   SizedBox(height: 10),
                   ImageInput(_selectImage),
                   SizedBox(height: 10),
-                  LocationInput(),
+                  LocationInput(_selectPlace),
                 ],
               ),
             ),
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -22,30 +22,32 @@ class PlacesListScreen extends StatelessWidget {
       body: FutureBuilder(
         future: Provider.of<GreatPlaces>(context, listen: false)
             .fetchAndSetPlaces(),
-        builder: (ctx, snapshot) =>
-            snapshot.connectionState == ConnectionState.waiting
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Consumer<GreatPlaces>(
-                    builder: (ctx, greatPlaces, child) =>
-                        greatPlaces.items.length <= 0
-                            ? child
-                            : ListView.builder(
-                                itemCount: greatPlaces.items.length,
-                                itemBuilder: (ctx, i) => ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundImage:
-                                        FileImage(greatPlaces.items[i].image),
-                                  ),
-                                  title: Text(greatPlaces.items[i].title),
-                                  onTap: () {},
-                                ),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                builder: (ctx, greatPlaces, child) =>
+                    greatPlaces.items.length <= 0
+                        ? child
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (ctx, i) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(greatPlaces.items[i].image),
                               ),
-                    child: Center(
-                      child: Text('No places added yet! Please add some'),
-                    ),
-                  ),
+                              title: Text(greatPlaces.items[i].title),
+                              subtitle:
+                                  Text(greatPlaces.items[i].location.address),
+                              onTap: () {},
+                            ),
+                          ),
+                child: Center(
+                  child: Text('No places added yet! Please add some'),
+                ),
+              ),
       ),
     );
   }
